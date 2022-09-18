48c446e5839acdfd25197cd812f7b61d74be4480
Justin <44349024+notjsouza@users.noreply.github.com>
2022-09-17 20:16:43 -0700

    Create Isomorphic-Strings.py

---
+++ Isomorphic-Strings.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        
+        s_t = {}
+        t_s = {}
+        
+        for s1, t1 in zip(s, t):
+            
+            if (s1 not in s_t) and (t1 not in t_s):
+                
+                s_t[s1] = t1
+                t_s[t1] = s1
+                
+            elif s_t.get(s1) != t1 or t_s.get(t1) != s1:
+                
+                return False
+        
+        return True
