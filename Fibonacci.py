858d03a6a9cf91b32332c2bc30bdf243baa9a0df
Justin <44349024+notjsouza@users.noreply.github.com>
2022-11-12 11:01:33 -0800

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -3,8 +3,6 @@ class Solution:
         
         d = [0, 1]
         
-        for i in range(0, n-1):
-            
-            d.append(d[i] + d[i+1])
+        for i in range(n-1): d.append(d[i] + d[i+1])
             
         return d[n]
