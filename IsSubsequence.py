e201274d18b783e7fdc9deada2a2583179b5a081
Justin <44349024+notjsouza@users.noreply.github.com>
2022-12-20 16:02:00 -0800

    Update IsSubsequence.py

--- IsSubsequence.py
+++ IsSubsequence.py
@@ -1,19 +1,9 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        
-        if not s:
-            return True
-        
-        sub = 0
-        
-        for i in t:
-            
-            if sub == len(s):
-                
-                return True
-            
-            if s[sub] == i:
-                
-                sub += 1
-                
-        return sub == len(s)
+
+        for char in t:
+
+            if s and char == s[0]: s = s[1:]
+
+        if not s: return True
+        return False
