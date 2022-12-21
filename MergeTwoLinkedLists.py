7ada56bf4bfebe17ba22f499073dc722d583532d
Justin <44349024+notjsouza@users.noreply.github.com>
2022-12-20 17:45:31 -0800

    Update MergeTwoLinkedLists.py

--- MergeTwoLinkedLists.py
+++ MergeTwoLinkedLists.py
@@ -1,39 +1,22 @@
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        
-        if not list1:
-            
-            return list2
-        
-        elif not list2:
-            
-            return list1
-        
-        head = ListNode(0, None)
-        ptr = head
-        
+
+        terminal = ListNode(0, None)
+        ptr = terminal
+
         while list1 and list2:
-            
-            if list1.val < list2.val:
-                
-                head.next = list1
+
+            if list1.val <= list2.val: 
+                ptr.next = list1
                 list1 = list1.next
-                l = True
-                
+
             else:
-                
-                head.next = list2
+                ptr.next = list2
                 list2 = list2.next
-                l = False
-                
-            head = head.next
-        
-        if list1:
-            
-            head.next = list1
-            
-        if list2:
-            
-            head.next = list2
-        
-        return ptr.next
+
+            ptr = ptr.next
+
+        if list1: ptr.next = list1
+        else: ptr.next = list2
+
+        return terminal.next
