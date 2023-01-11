7641c4d834c426f22387ccc531598b603b5c4d63
Justin <44349024+notjsouza@users.noreply.github.com>
2023-01-10 22:31:12 -0800

    Update MiddleOfLinkedList.py

--- MiddleOfLinkedList.py
+++ MiddleOfLinkedList.py
@@ -1,11 +1,11 @@
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-
-        slow, fast = head, head
+        
+        fast, slow = head, head
 
         while fast.next and fast.next.next:
 
-            fast = fast.next.next
             slow = slow.next
+            fast = fast.next.next
         
         return slow if not fast.next else slow.next
