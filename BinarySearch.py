dfb090216d1a660a1e06c17b58834b5477269dbd
Justin <44349024+notjsouza@users.noreply.github.com>
2023-01-13 18:00:50 -0800

    Update BinarySearch.py

--- BinarySearch.py
+++ BinarySearch.py
@@ -1,14 +1,15 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        left = 0
-        right = len(nums)-1
-        
-        while left <= right:
-            
-            i = left + (right - left)//2
+
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+
+            i = l + (r - l)//2
+
             if nums[i] == target: return i
             
-            if nums[i] < target: left = i + 1
-            else: right = i - 1
+            if nums[i] < target: l = i + 1
+            else: r = i - 1
 
         return -1
