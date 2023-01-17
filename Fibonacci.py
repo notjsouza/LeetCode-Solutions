d0ba651c3b48fd7c67405085a63a50e1786201df
Justin <44349024+notjsouza@users.noreply.github.com>
2023-01-17 15:57:09 -0800

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,8 +1,10 @@
 class Solution:
     def fib(self, n: int) -> int:
-        
-        d = [0, 1]
-        
-        for i in range(n-1): d.append(d[i] + d[i+1])
-            
-        return d[n]
+
+        arr = [0, 1]
+
+        for i in range(1, n):
+
+            arr.append(arr[i] + arr[i-1])
+
+        return arr[n]
