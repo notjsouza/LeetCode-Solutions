2331892fbd4c5514abae716d2adf590f89f79ce8
Justin <44349024+notjsouza@users.noreply.github.com>
2023-01-17 16:05:20 -0800

    Update MinCostClimbingStairs.py

--- MinCostClimbingStairs.py
+++ MinCostClimbingStairs.py
@@ -3,8 +3,6 @@ class Solution:
         
         cost.append(0)
         
-        for i in range(2, len(cost)):
-            
-            cost[i] += min(cost[i-1], cost[i-2])
+        for i in range(2, len(cost)): cost[i] += min(cost[i-1], cost[i-2])
                 
         return cost[len(cost)-1]
