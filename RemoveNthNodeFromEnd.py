3e6305c2fcc1109f566f30a4fc9c0c4a6b5b7bc7
Justin <44349024+notjsouza@users.noreply.github.com>
2023-03-12 04:04:10 -0700

    Update RemoveNthNodeFromEnd.py

--- RemoveNthNodeFromEnd.py
+++ RemoveNthNodeFromEnd.py
@@ -5,19 +5,16 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        
         sentinal = ListNode(0, head)
-        slow = fast = sentinal
+        fast = slow = sentinal
         i = 0
-        
+
         while fast:
-            
             if i > n: slow = slow.next
-            
             fast = fast.next
             i += 1
         
-        if slow.next: slow.next = slow.next.next 
+        if slow.next: slow.next = slow.next.next
         else: return head.next
-    
+
         return sentinal.next
