07a829266c02a8cc8ce28c16e409da69df4b361c
Justin <44349024+notjsouza@users.noreply.github.com>
2024-02-26 22:11:13 -0800

    Update InvertBinaryTree.py

--- InvertBinaryTree.py
+++ InvertBinaryTree.py
@@ -1,9 +1,7 @@
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         if not root: return
-        temp = root.right
-        root.right = root.left
-        root.left = temp
-        self.invertTree(root.right)
+        root.left, root.right = root.right, root.left
         self.invertTree(root.left)
+        self.invertTree(root.right)
         return root
